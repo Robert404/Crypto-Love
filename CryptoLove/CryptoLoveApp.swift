17f23c9322b89c7f06583634a8d3b33150974048
Robert <robners404@gmail.com>
2021-06-13 23:41:23 +0400

    Initial Commit

---
+++ CryptoLove/CryptoLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLoveApp.swift
+//  CryptoLove
+//
+//  Created by Robert Nersesyan on 13.06.21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
